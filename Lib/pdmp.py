13fef4d7fb988f8ba43e0547c2fd1948839d0e4d
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-04-05 03:19:37 -0700

    remove the fcntl stuff

--- Lib/pdmp.py
+++ Lib/pdmp.py
@@ -5,8 +5,6 @@
     Methods that allow interacting with a `pdmp` type.
 """
 
-import errno
-import fcntl
 import gc
 import mmap
 import os
@@ -32,6 +30,31 @@ def _unpack_n_longs(file_handle, n: int) -> Tuple[int, ...]:
     return struct.unpack('l' * n, file_handle.read(_SIZE_BYTES_LONG * n))
 
 
+class ByteConsistencyError(Exception):
+    pass
+
+
+@dataclass(frozen=True)
+class RelocationTable:
+    mmap: mmap.mmap
+    end_of_entries_offset: int
+
+    def _seek_to_entry(self, entry: 'DumpRelocationEntry') -> None:
+        self.mmap.seek(self.end_of_entries_offset + entry.base_offset)
+
+    def get_relocated_bytes(self, entry: 'DumpRelocationEntry') -> bytes:
+        self._seek_to_entry(entry)
+        return self.mmap.read(entry.extent)
+
+    def set_relocated_bytes(self, entry: 'DumpRelocationEntry', new_bytes: bytes) -> None:
+        if len(new_bytes) != entry.extent:
+            raise ByteConsistencyError(f'new relocated bytes {new_bytes} must be the correct size for entry {entry} (was: {len(new_bytes)})!')
+        self._seek_to_entry(entry)
+        written = self.mmap.write(new_bytes)
+        if written != entry.extent:
+            raise ByteConsistencyError(f'wrote fewer bytes ({written}) than expected ({entry.extent}) when updating a pdmp object relocation {entry}!')
+
+
 @dataclass(frozen=True)
 class DumpRelocationEntry:
     source_object: Any
@@ -52,12 +75,12 @@ class DumpRelocationEntry:
     def referents(self) -> List[Any]:
         return list(gc.get_referents(self.source_object))
 
-    def as_relocation_entry(self) -> bytes:
+    def pack_relocation_entry_bytes(self) -> bytes:
         return _pack_n_longs(
             self.volatile_memory_id, self.base_offset, self.extent, len(self.referents),
             *[id(obj) for obj in self.referents])
 
-    def as_relocatable_bytes(self) -> bytes:
+    def as_volatile_bytes(self) -> bytes:
         return gc.pdmp_write_relocatable_object(self.source_object, self.extent)
 
 
@@ -87,91 +110,79 @@ class Obarray:
 
 @dataclass
 class pdmp:
-    file_handle: Any
-    mmap: Optional[mmap.mmap]
+    file_path: Path
 
-    @property
-    def fd(self) -> int:
-        return self.file_handle.fileno()
+    def _get_mapped_memory_length(self) -> int:
+        return os.stat(self.file_path).st_size
 
-    @property
-    def dump_filename(self) -> Path:
-        return Path(self.file_handle.name)
-
-    @property
-    def mapped_memory_length(self) -> int:
-        return os.stat(self.fd).st_size
-
-    class Error(Exception):
-        """???"""
-
-    def __init__(self, file_handle) -> None:
-        assert file_handle.seekable(), f'{file_handle=} was not seekable!'
-        self.file_handle = file_handle
-        self.mmap = None
-
-    def is_mapped(self) -> bool:
-        return (self.mmap is not None) and (not self.mmap.closed)
+    def __init__(self, file_path: Path) -> None:
+        assert file_path.is_file(), f'pdmp file must point to existing path! was: {file_path}'
+        self.file_path = file_path
 
     def load(self) -> Any:
-        assert self.is_mapped(), f'pdmp object {self=} was not mapped when attempting to load root object!'
+        with self._read_handle() as read_mmap:
+            read_mmap.seek(0)
+            (num_objects_dumped,) = _unpack_n_longs(read_mmap, 1)
 
-        self.mmap.seek(0)
-        (num_objects_dumped,) = _unpack_n_longs(self.mmap, 1)
+            relocation_entries: List[LoadRelocationEntry] = []
+            for _ in range(num_objects_dumped):
+                volatile_memory_id, offset, extent, num_refs = _unpack_n_longs(read_mmap, 4)
+                referents = _unpack_n_longs(read_mmap, num_refs)
+                entry = LoadRelocationEntry(
+                    original_volatile_memory_id=volatile_memory_id,
+                    base_offset=offset,
+                    extent=extent,
+                    referents=referents,
+                )
+                relocation_entries.append(entry)
 
-        relocation_entries: List[LoadRelocationEntry] = []
-        for _ in range(num_objects_dumped):
-            volatile_memory_id, offset, extent, num_refs = _unpack_n_longs(self.mmap, 4)
-            referents = _unpack_n_longs(self.mmap, num_refs)
-            entry = LoadRelocationEntry(
-                original_volatile_memory_id=volatile_memory_id,
-                base_offset=offset,
-                extent=extent,
-                referents=referents,
-            )
-            relocation_entries.append(entry)
+            end_of_entries_offset = read_mmap.tell()
 
-        end_of_entries_offset = self.mmap.tell()
-
-        return relocation_entries
-
-    def __enter__(self) -> Any:
-        # Get a read lock on the current file.
-        fcntl.flock(self.fd, fcntl.LOCK_SH)
-
-        flags = mmap.MAP_SHARED
-        prot = mmap.PROT_READ
-        self.mmap = mmap.mmap(self.fd, self.mapped_memory_length, flags, prot)
-        assert self.is_mapped()
-        return self
-
-    def __exit__(self, _exc, _val, _tb) -> None:
-        self.mmap.close()
-        self.mmap = None
-        assert not self.is_mapped()
-
-        # Release the read lock on the current file.
-        fcntl.flock(self.fd, fcntl.LOCK_UN)
+            return relocation_entries
 
     @contextmanager
-    def _acquire_write_flock_nowait(self):
-        """Get a write lock on the current file."""
-        try:
-            fcntl.flock(self.fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        except OSError as e:
-            if e.errno == errno.EWOULDBLOCK:
-                raise self.Error(f'the file {self.file_handle=} was locked!!!') from e
-            raise
+    def _read_handle(self):
+        file_handle = open(self.file_path, 'rb')
+
+        flags = mmap.MAP_PRIVATE
+        prot = mmap.PROT_READ
+        length = self._get_mapped_memory_length()
+        mapping = mmap.mmap(file_handle.fileno(), length, flags, prot)
 
         try:
-            yield
+            yield mapping
         finally:
-            fcntl.flock(self.fd, fcntl.LOCK_UN)
+            mapping.close()
+            file_handle.close()
 
-    def dump(self, source_object: Any) -> List[DumpRelocationEntry]:
+    @contextmanager
+    def _acquire_write_file_handle(self, *, truncate: bool = True):
+        """Get a write lock on the current file."""
+        mode = 'wb' if truncate else 'r+b'
+        file_handle = open(self.file_path, mode)
+
+        try:
+            file_handle.seek(0)
+            yield file_handle
+        finally:
+            file_handle.close()
+
+    @contextmanager
+    def _open_write_mapping(self, fd: int):
+        flags = mmap.MAP_PRIVATE
+        prot = mmap.PROT_WRITE
+        length = self._get_mapped_memory_length()
+        mapping = mmap.mmap(fd, length, flags, prot)
+
+        try:
+            yield mapping
+        finally:
+            mapping.close()
+
+    def _dump_volatile_memory(self, source_object: Any) -> Tuple[int, bytes, List[DumpRelocationEntry]]:
         obarray = Obarray()
         dump_queue = Queue()
-        dumped_infos: List[DumpRelocationEntry] = []
+        dump_entries: List[DumpRelocationEntry] = []
 
         dump_queue.put(source_object)
 
@@ -195,21 +206,44 @@ class pdmp:
                 dump_queue.put(ref)
 
             # Return the list of relocation entries.
-            dumped_infos.append(entry)
+            dump_entries.append(entry)
 
             # Bump the offset.
             total_offset += entry.extent
 
+        all_file_bytes = b''
+
+        # Write entries to a `bytes`.
+        num_objects_dumped = len(dump_entries)
+        all_file_bytes += _pack_n_longs(num_objects_dumped)
+
+        for entry in dump_entries:
+            all_file_bytes += entry.pack_relocation_entry_bytes()
+
+        end_of_entries_offset = len(all_file_bytes)
+
+        for entry in dump_entries:
+            all_file_bytes += entry.as_volatile_bytes()
+
+        return (end_of_entries_offset, all_file_bytes, dump_entries)
+
+    def dump(self, source_object: Any) -> List[DumpRelocationEntry]:
+        end_of_entries_offset, all_file_bytes, dump_entries = self._dump_volatile_memory(source_object)
+
         # Write entries to the file.
-        with self._acquire_write_flock_nowait():
-            self.file_handle.seek(0)
+        with self._acquire_write_file_handle(truncate=True) as file_handle:
+            written = file_handle.write(all_file_bytes)
+            if written != len(all_file_bytes):
+                raise ByteConsistencyError(f'written bytes {written} was not equal to expected length of full pdmp file: {len(all_file_bytes)}!')
 
-            num_objects_dumped = len(dumped_infos)
-            self.file_handle.write(struct.pack('l', num_objects_dumped))
+        with self._acquire_write_file_handle(truncate=False) as file_handle:
+            with self._open_write_mapping(file_handle.fileno()) as mapping:
+                relocation_table = RelocationTable(
+                    mmap=mapping,
+                    end_of_entries_offset=end_of_entries_offset,
+                )
+                for entry in dump_entries:
+                    relocated_bytes = relocation_table.get_relocated_bytes(entry)
+                    import pdb; pdb.set_trace()
 
-            for info in dumped_infos:
-                self.file_handle.write(info.as_relocation_entry())
-            for info in dumped_infos:
-                self.file_handle.write(info.as_relocatable_bytes())
-
-        return dumped_infos
+        return dump_entries
